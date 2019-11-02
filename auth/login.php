9e11f7befb653fe87f844583a32ae6b9500dc936
v24s08 <sarathsekaran2007@gmail.com>
2019-11-02 20:02:37 +0530

    Sign up backend added

--- auth/login.php
+++ auth/login.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,17 +13,16 @@
     <!-- Main css -->
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
 
     <div class="main">
- <!-- Sing in  Form -->
+        <!-- Sing in  Form -->
         <section class="sign-in">
             <div class="container">
                 <div class="signin-content">
                     <div class="signin-image">
-                        <!--<figure><img src="images/Kentex-Free-Account.png" alt="sing up image"></figure>-->
-                        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-                        <a href="index.html" class="signup-image-link">Create an account</a>
+                        <figure><img src="images/sign-up-masters-in-cash-300x300.png" alt="sing up image"></figure>
                     </div>
 
                     <div class="signin-form">
@@ -30,18 +30,18 @@
                         <form method="POST" class="register-form" id="login-form">
                             <div class="form-group">
                                 <label for="email"><i class="zmdi zmdi-email"></i></label>
-                                <input type="email" name="email" id="email" placeholder="Your Email"/>
+                                <input type="email" name="email" id="email" placeholder="Your Email" />
                             </div>
                             <div class="form-group">
                                 <label for="your_pass"><i class="zmdi zmdi-lock"></i></label>
-                                <input type="password" name="your_pass" id="your_pass" placeholder="Password"/>
+                                <input type="password" name="your_pass" id="your_pass" placeholder="Password" />
                             </div>
                             <div class="form-group">
                                 <input type="checkbox" name="remember-me" id="remember-me" class="agree-term" />
                                 <label for="remember-me" class="label-agree-term"><span><span></span></span>Remember me</label>
                             </div>
                             <div class="form-group form-button">
-                                <input type="submit" name="signin" id="signin" class="form-submit" value="Log in"/>
+                                <input type="submit" name="signin" id="signin" class="form-submit" value="Log in" />
                             </div>
                         </form>
                         <div class="social-login">
@@ -54,6 +54,7 @@
                         </div>
                     </div>
                 </div>
+                <a href="index.html" class="signup-image-link">Create an account</a><br>
             </div>
         </section>
 
@@ -63,4 +64,5 @@
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="js/main.js"></script>
 </body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+
 </html>
--- auth/sign-up.php
+++ auth/sign-up.php
@@ -24,29 +24,29 @@
                 <div class="signup-content">
                     <div class="signup-form">
                         <h2 class="form-title">Sign Up</h2>
-                        <form method="POST" class="register-form" id="register-form">
+                        <form method="POST" action="sign-up.php" onsubmit="return password_match(this);" class="register-form" id="register-form">
                             <div class="form-group">
                                 <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                                <input type="text" name="name" id="name" placeholder="First Name" />
+                                <input type="text" name="fname" id="fname" placeholder="First Name" required/>
                             </div>
                             <div class="form-group">
                                 <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                                <input type="text" name="name" id="name" placeholder="Last Name" />
+                                <input type="text" name="lname" id="lname" placeholder="Last Name" required />
                             </div>
                             <div class="form-group">
                                 <label for="email"><i class="zmdi zmdi-email"></i></label>
-                                <input type="email" name="email" id="email" placeholder="Your Email" />
+                                <input type="email" name="email" id="email" placeholder="Your Email" required/>
                             </div>
                             <div class="form-group">
                                 <label for="pass"><i class="zmdi zmdi-lock"></i></label>
-                                <input type="password" name="pass" id="pass" placeholder="Password" />
+                                <input type="password" name="password" id="password" placeholder="Password" required/>
                             </div>
                             <div class="form-group">
                                 <label for="re-pass"><i class="zmdi zmdi-lock-outline"></i></label>
-                                <input type="password" name="re_pass" id="re_pass" placeholder="Repeat your password" />
+                                <input type="password" name="re_pass" id="re_pass" placeholder="Repeat your password" required/>
                             </div>
                             <div class="form-group">
-                                <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
+                                <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" required />
                                 <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all statements in <a href="#" class="term-service">Terms of service</a></label>
                             </div>
                             <div class="form-group form-button">
@@ -55,19 +55,66 @@
                         </form>
                     </div>
                     <div class="signup-image">
-                    <figure><img src="images/sign-up-masters-in-cash-300x300.png" alt="sing up image"></figure>
+                        <figure><img src="images/sign-up-masters-in-cash-300x300.png" alt="sing up image"></figure>
                     </div>
                 </div>
-                <a href="signin.html" class="signup-image-link">I am already member</a><br>
+                <a href="login.php" class="signup-image-link">I am already member</a><br>
             </div>
         </section>
 
 
     </div>
 
+    <script>
+        function password_match() {
+            var password = document.getElementById('password').value;
+            var re_pass = document.getElementById('re_pass').value;
+
+            if (password !== re_pass) {
+                alert('Password not matched');
+                return false;
+            }
+            return true;
+        }
+    </script>
     <!-- JS -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="js/main.js"></script>
-</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+</body>
 
 </html>
+
+<?php
+
+if (isset($_POST['signup'])) {
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "whd";
+
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+
+    $firstname = $_POST['fname'];
+    $lastname = $_POST['lname'];
+    $email = $_POST['email'];
+    $password = sha1($_POST['password']);
+
+
+    // prepare and bind
+    $stmt = $conn->prepare("INSERT INTO users (fname, lname, email, password) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("ssss", $firstname, $lastname, $email, $password);
+    //execute the statement
+    $stmt->execute();
+    //close connections
+    $stmt->close();
+    $conn->close();
+
+    header("Location: login.php");
+}
